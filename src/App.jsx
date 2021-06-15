e88cd5f97dc8c43c982c764016c2e64d3be841fa
VicDario <26585356+VicDario@users.noreply.github.com>
2021-06-15 17:55:09 -0400

    Fetch added. Push items to API completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
@@ -7,19 +7,72 @@ function App() {
 	let [toDoList, setList] = useState([]);
 	let [numberElements, setNumber] = useState(0);
 
-	const addItem = (e) => {
-		if(e.code === "Enter"){
-			setList((prevState) =>{
-				return [...prevState,{id: numberElements, label: e.target.value}];
-			})
-			setNumber(numberElements + 1);
-		}
+	let input = useRef();
+
+	let url = 'https://assets.breatheco.de/apis/fake/todos/user/vicdario';
+
+	useEffect(()=>{
+		getList(url);
+	}, [])
+
+
+/* 	const createAPI = (url, options = {}) => {
+		fetch(url, {
+			method: 'POST', // or 'PUT'
+			body: JSON.stringify({}), // data can be `string` or {object}!
+			headers:{
+			  'Content-Type': 'application/json'
+			}
+		  }).then(res => res.json())
+		  .catch(error => console.error('Error:', error))
+		  .then(response => console.log('Success:', response));
+		  
+    } */
+
+	const getList = async (url) => {
+        try {
+            const response = await fetch(url, { 
+				method: 'GET',
+				headers: { 'Content-Type': 'application/json' }
+			});
+            if (response.status === 404) throw new Error("Pagina No encontrada");
+            const data = await response.json();
+        	setList([...data]);
+			setNumber(data.lenght);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+	const PushItemList = async (url, item) => {
+        try {
+            const response = await fetch(url, {
+				method: 'PUT',
+				body: JSON.stringify(item),
+				headers: { 'Content-Type': 'application/json' }
+			});
+            if (response.status === 404) throw new Error("Pagina No encontrada");
+            const data = await response.json();
+			console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		let item = [
+			...toDoList,
+			{"label": input.current.value, "done": false}
+		];
+		console.log(input.current.value);
+		PushItemList('https://assets.breatheco.de/apis/fake/todos/user/vicdario', item)
 	}
 
 	const deleteItem = (id) => {
-		let listUpdated = toDoList.filter(item => item.id !== id);
+		/* let listUpdated = toDoList.filter(item => item.id !== id);
 		setList(listUpdated);
-		setNumber(numberElements-1);
+		setNumber(numberElements-1); */
 	}
 
 	return (
@@ -28,18 +81,21 @@ function App() {
 			<div className="row my-5">
 				<div className="col-md-6 offset-md-3">
 					<ul className="list-group">
-						<li className="list-group-item"><input className="px-4 py-3" type="text" onKeyPress={addItem} placeholder="What needs to be done?" /></li>
+						<li className="list-group-item">	
+							<form action="submit" onSubmit={handleSubmit} >
+								<input ref={input} type="text" className="px-4 py-3" placeholder="What needs to be done?" />
+							</form>
+						</li>
 						{
-							numberElements > 0 ?
-							toDoList.map((item, key) =>{
+							!!toDoList ?
+							toDoList.map((item, key) => {
 								return <li id={item.id} className="list-group-item d-flex justify-content-between align-items-center" key={key}>
-									{item.label}<button type="button" className="button-delete" onClick={()=>deleteItem(item.id)}><FontAwesomeIcon id={key} icon={faTrashAlt} /></button></li>
+									{item.label}<FontAwesomeIcon className="button-delete" onClick={()=>deleteItem(item.id)} id={key} icon={faTrashAlt} /></li>
 							}) :
 							<li className="list-group-item">No tasks, add a task</li>
 						}
 						<li className="list-group-item shadows"><small className="text-muted">{numberElements} items left</small></li>
 					</ul>
-					
 				</div>
 			</div>
 		</div>
